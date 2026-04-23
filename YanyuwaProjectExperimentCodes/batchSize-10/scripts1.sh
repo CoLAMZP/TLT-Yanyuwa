9e06aa2ed97fecb743ee665cea9f8c800f7769d5
CoLAMZP <164866285+CoLAMZP@users.noreply.github.com>
2026-04-23 19:17:45 +1000

    Delete YanyuwaProjectExperimentCodes/batchSize-10 directory

--- YanyuwaProjectExperimentCodes/batchSize-10/scripts1.sh
+++
@@ -1,4 +0,0 @@
-
-python -u main_code.py --num_epochs 150 --task pos --learning_rate 0.00001 --data_file data10.xlsx --active_num 10 --run_ix bert_ner_1.0 --model_name bert-base-multilingual-cased --mtr 2   >mbert_2_pos.txt
-
-python -u main_code.py --num_epochs 150 --task ner --learning_rate 0.00001 --data_file data10.xlsx --active_num 10 --run_ix bert_ner_1.0 --model_name bert-base-cased --mtr 2   >bert_2_ner.txt

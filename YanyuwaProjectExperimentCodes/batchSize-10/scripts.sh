d88e3d3b2eeb95edb3e4a1ec2581e09ee6b82e72
CoLAMZP <164866285+CoLAMZP@users.noreply.github.com>
2026-04-23 19:17:13 +1000

    Add files via upload

---
+++ YanyuwaProjectExperimentCodes/batchSize-10/scripts.sh
@@ -0,0 +1,165 @@
+
+
+
+
+
+
+
+python -u main_code.py --num_epochs 20 --task pos --learning_rate 0.00001 --data_file data10.xlsx --active_num 10 --run_ix bert_ner_1.0 --model_name bert-base-cased --mtr 0   >bert_random_pos.txt
+
+python -u main_code.py --num_epochs 20 --task pos --learning_rate 0.00001 --data_file data10.xlsx --active_num 10 --run_ix bert_ner_1.0 --model_name roberta-base --mtr 0   >roberta_random_pos.txt
+
+python -u main_code.py --num_epochs 20 --task pos --learning_rate 0.00001 --data_file data10.xlsx --active_num 10 --run_ix bert_ner_1.0 --model_name bert-base-multilingual-cased --mtr 0   >mbert_random_pos.txt
+
+
+
+
+python -u main_code.py --num_epochs 20 --task pos --learning_rate 0.00001 --data_file data10.xlsx --active_num 10 --run_ix bert_ner_1.0 --model_name bert-base-cased --mtr -1   >bert_-1_pos.txt
+
+python -u main_code.py --num_epochs 20 --task pos --learning_rate 0.00001 --data_file data10.xlsx --active_num 10 --run_ix bert_ner_1.0 --model_name roberta-base --mtr -1   >roberta_-1_pos.txt
+
+python -u main_code.py --num_epochs 20 --task pos --learning_rate 0.00001 --data_file data10.xlsx --active_num 10 --run_ix bert_ner_1.0 --model_name bert-base-multilingual-cased --mtr -1   >mbert_-1_pos.txt
+
+
+
+
+
+python -u main_code.py --num_epochs 20 --task pos --learning_rate 0.00001 --data_file data10.xlsx --active_num 10 --run_ix bert_ner_1.0 --model_name bert-base-cased --mtr 1   >bert_1_pos.txt
+
+python -u main_code.py --num_epochs 20 --task pos --learning_rate 0.00001 --data_file data10.xlsx --active_num 10 --run_ix bert_ner_1.0 --model_name roberta-base --mtr 1   >roberta_1_pos.txt
+
+python -u main_code.py --num_epochs 20 --task pos --learning_rate 0.00001 --data_file data10.xlsx --active_num 10 --run_ix bert_ner_1.0 --model_name bert-base-multilingual-cased --mtr 1   >mbert_1_pos.txt
+
+
+
+
+
+python -u main_code.py --num_epochs 20 --task pos --learning_rate 0.00001 --data_file data10.xlsx --active_num 10 --run_ix bert_ner_1.0 --model_name bert-base-cased --mtr 2   >bert_2_pos.txt
+
+python -u main_code.py --num_epochs 20 --task pos --learning_rate 0.00001 --data_file data10.xlsx --active_num 10 --run_ix bert_ner_1.0 --model_name roberta-base --mtr 2   >roberta_2_pos.txt
+
+python -u main_code.py --num_epochs 20 --task pos --learning_rate 0.00001 --data_file data10.xlsx --active_num 10 --run_ix bert_ner_1.0 --model_name bert-base-multilingual-cased --mtr 2   >mbert_2_pos.txt
+
+
+
+
+
+python -u main_code.py --num_epochs 20 --task pos --learning_rate 0.00001 --data_file data10.xlsx --active_num 10 --run_ix bert_ner_1.0 --model_name bert-base-cased --mtr 3   >bert_3_pos.txt
+
+python -u main_code.py --num_epochs 20 --task pos --learning_rate 0.00001 --data_file data10.xlsx --active_num 10 --run_ix bert_ner_1.0 --model_name roberta-base --mtr 3   >roberta_3_pos.txt
+
+python -u main_code.py --num_epochs 20 --task pos --learning_rate 0.00001 --data_file data10.xlsx --active_num 10 --run_ix bert_ner_1.0 --model_name bert-base-multilingual-cased --mtr 3   >mbert_3_pos.txt
+
+
+
+
+
+python -u main_code.py --num_epochs 20 --task pos --learning_rate 0.00001 --data_file data10.xlsx --active_num 10 --run_ix bert_ner_1.0 --model_name bert-base-cased --mtr 4   >bert_4_pos.txt
+
+python -u main_code.py --num_epochs 20 --task pos --learning_rate 0.00001 --data_file data10.xlsx --active_num 10 --run_ix bert_ner_1.0 --model_name roberta-base --mtr 4   >roberta_4_pos.txt
+
+python -u main_code.py --num_epochs 20 --task pos --learning_rate 0.00001 --data_file data10.xlsx --active_num 10 --run_ix bert_ner_1.0 --model_name bert-base-multilingual-cased --mtr 4   >mbert_4_pos.txt
+
+
+
+
+
+
+python -u main_code.py --num_epochs 20 --task ner --learning_rate 0.00001 --data_file data10.xlsx --active_num 10 --run_ix bert_ner_1.0 --model_name bert-base-cased --mtr 0   >bert_random_ner.txt
+
+python -u main_code.py --num_epochs 20 --task ner --learning_rate 0.00001 --data_file data10.xlsx --active_num 10 --run_ix bert_ner_1.0 --model_name roberta-base --mtr 0   >roberta_random_ner.txt
+
+python -u main_code.py --num_epochs 20 --task ner --learning_rate 0.00001 --data_file data10.xlsx --active_num 10 --run_ix bert_ner_1.0 --model_name bert-base-multilingual-cased --mtr 0   >mbert_random_ner.txt
+
+
+
+
+python -u main_code.py --num_epochs 20 --task ner --learning_rate 0.00001 --data_file data10.xlsx --active_num 10 --run_ix bert_ner_1.0 --model_name bert-base-cased --mtr -1   >bert_-1_ner.txt
+
+python -u main_code.py --num_epochs 20 --task ner --learning_rate 0.00001 --data_file data10.xlsx --active_num 10 --run_ix bert_ner_1.0 --model_name roberta-base --mtr -1   >roberta_-1_ner.txt
+
+python -u main_code.py --num_epochs 20 --task ner --learning_rate 0.00001 --data_file data10.xlsx --active_num 10 --run_ix bert_ner_1.0 --model_name bert-base-multilingual-cased --mtr -1   >mbert_-1_ner.txt
+
+
+
+
+
+python -u main_code.py --num_epochs 20 --task ner --learning_rate 0.00001 --data_file data10.xlsx --active_num 10 --run_ix bert_ner_1.0 --model_name bert-base-cased --mtr 1   >bert_1_ner.txt
+
+python -u main_code.py --num_epochs 20 --task ner --learning_rate 0.00001 --data_file data10.xlsx --active_num 10 --run_ix bert_ner_1.0 --model_name roberta-base --mtr 1   >roberta_1_ner.txt
+
+python -u main_code.py --num_epochs 20 --task ner --learning_rate 0.00001 --data_file data10.xlsx --active_num 10 --run_ix bert_ner_1.0 --model_name bert-base-multilingual-cased --mtr 1   >mbert_1_ner.txt
+
+
+
+
+
+python -u main_code.py --num_epochs 20 --task ner --learning_rate 0.00001 --data_file data10.xlsx --active_num 10 --run_ix bert_ner_1.0 --model_name bert-base-cased --mtr 2   >bert_2_ner.txt
+
+python -u main_code.py --num_epochs 20 --task ner --learning_rate 0.00001 --data_file data10.xlsx --active_num 10 --run_ix bert_ner_1.0 --model_name roberta-base --mtr 2   >roberta_2_ner.txt
+
+python -u main_code.py --num_epochs 20 --task ner --learning_rate 0.00001 --data_file data10.xlsx --active_num 10 --run_ix bert_ner_1.0 --model_name bert-base-multilingual-cased --mtr 2   >mbert_2_ner.txt
+
+
+
+
+
+python -u main_code.py --num_epochs 20 --task ner --learning_rate 0.00001 --data_file data10.xlsx --active_num 10 --run_ix bert_ner_1.0 --model_name bert-base-cased --mtr 3   >bert_3_ner.txt
+
+python -u main_code.py --num_epochs 20 --task ner --learning_rate 0.00001 --data_file data10.xlsx --active_num 10 --run_ix bert_ner_1.0 --model_name roberta-base --mtr 3   >roberta_3_ner.txt
+
+python -u main_code.py --num_epochs 20 --task ner --learning_rate 0.00001 --data_file data10.xlsx --active_num 10 --run_ix bert_ner_1.0 --model_name bert-base-multilingual-cased --mtr 3   >mbert_3_ner.txt
+
+
+
+
+
+python -u main_code.py --num_epochs 20 --task ner --learning_rate 0.00001 --data_file data10.xlsx --active_num 10 --run_ix bert_ner_1.0 --model_name bert-base-cased --mtr 4   >bert_4_ner.txt
+
+python -u main_code.py --num_epochs 20 --task ner --learning_rate 0.00001 --data_file data10.xlsx --active_num 10 --run_ix bert_ner_1.0 --model_name roberta-base --mtr 4   >roberta_4_ner.txt
+
+python -u main_code.py --num_epochs 20 --task ner --learning_rate 0.00001 --data_file data10.xlsx --active_num 10 --run_ix bert_ner_1.0 --model_name bert-base-multilingual-cased --mtr 4   >mbert_4_ner.txt
+
+
+
+
+
+
+python -u main_code.py --num_epochs 20 --task pos --learning_rate 0.00001 --data_file data10.xlsx --active_num 10 --run_ix bert_ner_1.0 --model_name bert-base-cased --mtr -2   >bert_kmeans_pos.txt
+
+python -u main_code.py --num_epochs 20 --task pos --learning_rate 0.00001 --data_file data10.xlsx --active_num 10 --run_ix bert_ner_1.0 --model_name roberta-base --mtr -2   >roberta_kmeans_pos.txt
+
+python -u main_code.py --num_epochs 20 --task pos --learning_rate 0.00001 --data_file data10.xlsx --active_num 10 --run_ix bert_ner_1.0 --model_name bert-base-multilingual-cased --mtr -2   >mbert_kmeans_pos.txt
+
+
+
+
+python -u main_code.py --num_epochs 20 --task pos --learning_rate 0.00001 --data_file data10.xlsx --active_num 10 --run_ix bert_ner_1.0 --model_name bert-base-cased --mtr -3   >bert_length_pos.txt
+
+python -u main_code.py --num_epochs 20 --task pos --learning_rate 0.00001 --data_file data10.xlsx --active_num 10 --run_ix bert_ner_1.0 --model_name roberta-base --mtr -3   >roberta_length_pos.txt
+
+python -u main_code.py --num_epochs 20 --task pos --learning_rate 0.00001 --data_file data10.xlsx --active_num 10 --run_ix bert_ner_1.0 --model_name bert-base-multilingual-cased --mtr -3   >mbert_length_pos.txt
+
+
+
+
+python -u main_code.py --num_epochs 20 --task ner --learning_rate 0.00001 --data_file data10.xlsx --active_num 10 --run_ix bert_ner_1.0 --model_name bert-base-cased --mtr -2   >bert_kmeans_ner.txt
+
+python -u main_code.py --num_epochs 20 --task ner --learning_rate 0.00001 --data_file data10.xlsx --active_num 10 --run_ix bert_ner_1.0 --model_name roberta-base --mtr -2   >roberta_kmeans_ner.txt
+
+python -u main_code.py --num_epochs 20 --task ner --learning_rate 0.00001 --data_file data10.xlsx --active_num 10 --run_ix bert_ner_1.0 --model_name bert-base-multilingual-cased --mtr -2   >mbert_kmeans_ner.txt
+
+
+
+
+python -u main_code.py --num_epochs 20 --task ner --learning_rate 0.00001 --data_file data10.xlsx --active_num 10 --run_ix bert_ner_1.0 --model_name bert-base-cased --mtr -3   >bert_length_ner.txt
+
+python -u main_code.py --num_epochs 20 --task ner --learning_rate 0.00001 --data_file data10.xlsx --active_num 10 --run_ix bert_ner_1.0 --model_name roberta-base --mtr -3   >roberta_length_ner.txt
+
+python -u main_code.py --num_epochs 20 --task ner --learning_rate 0.00001 --data_file data10.xlsx --active_num 10 --run_ix bert_ner_1.0 --model_name bert-base-multilingual-cased --mtr -3   >mbert_length_ner.txt
+
+
+
+
+
+
